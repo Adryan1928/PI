b9cc5464dda94a92cad1ba8906b5d78c49ddb75a
Adryan1928 <adryanoliveira1928@gmail.com>
2024-08-30 22:38:21 -0300

    Add blog

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import blog
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', blog, name='blog'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
